d7523bb0ccaf9fa129d6a46acf0654b05541f430
Mark A. Tsuchida <matsuchida@wisc.edu>
2022-09-07 11:34:05 -0500

    Reduce lock contention in buffer
    Avoid re-locking between every emitted element.

--- include/FLIMEvents/Buffer.hpp
+++ include/FLIMEvents/Buffer.hpp
@@ -190,9 +190,27 @@ template <typename E, typename D> class BufferEvent {
     std::mutex mutex;
     std::condition_variable hasItemCondition; // item = event or end
 
-    std::queue<E> queue;
+    std::queue<E> shared_queue;
     bool streamEnded = false;
     std::exception_ptr queued_error;
+
+    // To reduce lock contention on the shared_queue, we use a second queue
+    // that is accessed only by the emitting thread and is not protected by the
+    // mutex. Events in the shared_queue are transferred in bulk to the
+    // emit_queue while the mutex is held.
+    // This means that the mutex does not need to be acquired between every
+    // event emitted, so the producer will be less likely to block when the
+    // data rate is momentarily high, and the consumer will be less likely to
+    // block while catching up on buffered events.
+    // The emit_queue is always empty at resting, but we keep it in a data
+    // member in order to reuse allocated memory.
+    // Note: Swapping the queues is not theoretically necessary when the queue
+    // is based on std::deque (which never invalidates iterators): the range of
+    // items could safely be accessed with the mutex released given that no
+    // other thread will remove elements. But std::queue only gives access to
+    // front() and in any case we may want to use something more efficient that
+    // deque in the future (which may reallocate), so let's keep it simple.
+    std::queue<E> emit_queue; // Invariant: always empty
 
     D downstream;
 
@@ -212,7 +230,7 @@ template <typename E, typename D> class BufferEvent {
                 return;
 
             try {
-                queue.push(event);
+                shared_queue.push(event);
             } catch (std::exception const &) {
                 streamEnded = true;
                 queued_error = std::current_exception();
@@ -241,38 +259,30 @@ template <typename E, typename D> class BufferEvent {
      * and all events have been emitted downstream.
      */
     void PumpDownstream() noexcept {
-        // Removal from the queue only takes place here; because std::deque
-        // does not move elements, we can safely access the contents of
-        // queue.front() with the mutex unlocked.
-
         std::unique_lock lock(mutex);
 
         for (;;) {
-            while (queue.empty() && !streamEnded)
+            while (shared_queue.empty() && !streamEnded)
                 hasItemCondition.wait(lock);
 
-            bool isEnd = queue.empty();
-
-            std::exception_ptr error;
-            E const *event = nullptr;
-
-            if (isEnd)
+            if (shared_queue.empty()) { // Implying stream ended
+                std::exception_ptr error;
                 std::swap(error, queued_error);
-            else
-                event = &queue.front();
-
-            lock.unlock();
-
-            if (isEnd) {
+                lock.unlock();
                 downstream.HandleEnd(error);
                 return;
             }
 
-            downstream.HandleEvent(*event);
+            emit_queue.swap(shared_queue);
+
+            lock.unlock();
+
+            while (!emit_queue.empty()) {
+                downstream.HandleEvent(emit_queue.front());
+                emit_queue.pop();
+            }
 
             lock.lock();
-
-            queue.pop();
         }
     }
 };
