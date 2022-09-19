05cb79823b44013de535700e8d487810d86e87a0
Mark A. Tsuchida <matsuchida@wisc.edu>
2022-09-19 11:41:54 -0500

    Rename and reorder template parameters

--- include/flimevt/count.hpp
+++ include/flimevt/count.hpp
@@ -16,7 +16,7 @@ namespace flimevt {
 
 namespace internal {
 
-template <typename EIn, typename EReset, typename EOut, bool EmitAfter,
+template <typename ETick, typename EFire, typename EReset, bool EmitAfter,
           typename D>
 class count_event {
     std::uint64_t count = 0;
@@ -32,10 +32,10 @@ class count_event {
         assert(limit > 0);
     }
 
-    void handle_event(EIn const &event) noexcept {
+    void handle_event(ETick const &event) noexcept {
         if constexpr (!EmitAfter) {
             if (count == thresh)
-                downstream.handle_event(EOut{event.macrotime});
+                downstream.handle_event(EFire{event.macrotime});
         }
 
         downstream.handle_event(event);
@@ -43,7 +43,7 @@ class count_event {
 
         if constexpr (EmitAfter) {
             if (count == thresh)
-                downstream.handle_event(EOut{event.macrotime});
+                downstream.handle_event(EFire{event.macrotime});
         }
 
         if (count == limit)
@@ -70,15 +70,15 @@ class count_event {
  * \brief Create a processor that counts a specific event and emits an event
  * when the count reaches a threshold.
  *
- * All events (including \c EIn and \c EReset) are passed through.
+ * All events (including \c ETick and \c EReset) are passed through.
  *
- * \c EIn and \c EOut must have a macrotime field and \c EOut must be
- * brace-initializable with macrotime (as in \c EOut{123} ).
+ * \c ETick and \c EFire must have a macrotime field and \c EFire must be
+ * brace-initializable with macrotime (as in \c EFire{123} ).
  *
- * The count is incremented as \c EIn is passed through. Just before or after
+ * The count is incremented as \c ETick is passed through. Just before or after
  * that (depending on whether \c EmitAfter is false or true), the count is
- * compared to the \e threshold and if equal, \c EOut is emitted, with its
- * macrotime set equal to the \c EIn that triggered it.
+ * compared to the \e threshold and if equal, \c EFire is emitted, with its
+ * macrotime set equal to the \c ETick that triggered it.
  *
  * After incrementing the count and processing the threshold, if the count
  * equals the \e limit, then the count is reset to zero. Automatic resetting
@@ -86,32 +86,32 @@ class count_event {
  *
  * The \e limit must be positive (a zero limit would imply automatically
  * resetting without any input, which doesn't make sense). When \c EmitAfter is
- * false, \e threshold should be less than the limit; otherwise \c EOut is
+ * false, \e threshold should be less than the limit; otherwise \c EFire is
  * never emitted. When \c EmitAfter is true, \c threshold should be greater
- * than zero and less than or equal to the limit; otherwise \c EOut is never
+ * than zero and less than or equal to the limit; otherwise \c EFire is never
  * emitted.
  *
  * When an \c EReset is received (and passed through), the count is reset to
- * zero. No \c EOut is emitted on reset, but if \c EmitAfter is false and the
- * threshold is set to zero, then an \c EOut is emitted on the next \c EIn
+ * zero. No \c EFire is emitted on reset, but if \c EmitAfter is false and the
+ * threshold is set to zero, then an \c EFire is emitted on the next \c ETick
  * received.
  *
- * \tparam EIn the event type to count
+ * \tparam ETick the event type to count
+ * \tparam EFire the event type to emit when the count reaches the threshold
  * \tparam EReset an event type that causes the count to be reset to zero
- * \tparam EOut the event type to emit when the count reaches the threshold
- * \tparam EmitAfter whether to emit \c EOut after passing through \c EIn
+ * \tparam EmitAfter whether to emit \c EFire after passing through \c ETick
  * \tparam D downstream processor type
- * \param threshold the count value at which to emit \c EOut
+ * \param threshold the count value at which to emit \c EFire
  * \param limit the count value at which to reset to zero (set to \c
  * std::uint64_t{-1} if automatic reset is not desired); must be positive
  * \param downstream downstream processor (moved out)
  * \return count-event processor
  */
-template <typename EIn, typename EReset, typename EOut, bool EmitAfter,
+template <typename ETick, typename EFire, typename EReset, bool EmitAfter,
           typename D>
 auto count_event(std::uint64_t threshold, std::uint64_t limit,
                  D &&downstream) {
-    return internal::count_event<EIn, EReset, EOut, EmitAfter, D>(
+    return internal::count_event<ETick, EFire, EReset, EmitAfter, D>(
         threshold, limit, std::forward<D>(downstream));
 }
 
--- test/count.cpp
+++ test/count.cpp
@@ -14,34 +14,34 @@
 
 using namespace flimevt;
 
-using input_event = timestamped_test_event<0>;
-using output_event = timestamped_test_event<1>;
+using tick_event = timestamped_test_event<0>;
+using fire_event = timestamped_test_event<1>;
 using reset_event = timestamped_test_event<2>;
 using misc_event = timestamped_test_event<3>;
 
 TEST_CASE("Count event", "[count_event]") {
     auto out = capture_output<
-        event_set<input_event, output_event, reset_event, misc_event>>();
+        event_set<tick_event, fire_event, reset_event, misc_event>>();
 
     SECTION("Threshold 0, limit 1") {
         SECTION("Emit before") {
             auto in =
-                feed_input<event_set<input_event, reset_event, misc_event>>(
-                    count_event<input_event, reset_event, output_event, false>(
+                feed_input<event_set<tick_event, reset_event, misc_event>>(
+                    count_event<tick_event, fire_event, reset_event, false>(
                         0, 1, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(output_event{42}));
-            REQUIRE(out.check(input_event{42}));
-            in.feed(input_event{43});
-            REQUIRE(out.check(output_event{43}));
-            REQUIRE(out.check(input_event{43}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(fire_event{42}));
+            REQUIRE(out.check(tick_event{42}));
+            in.feed(tick_event{43});
+            REQUIRE(out.check(fire_event{43}));
+            REQUIRE(out.check(tick_event{43}));
             in.feed(reset_event{44});
             REQUIRE(out.check(reset_event{44}));
-            in.feed(input_event{45});
-            REQUIRE(out.check(output_event{45}));
-            REQUIRE(out.check(input_event{45}));
+            in.feed(tick_event{45});
+            REQUIRE(out.check(fire_event{45}));
+            REQUIRE(out.check(tick_event{45}));
             in.feed(misc_event{46});
             REQUIRE(out.check(misc_event{46}));
             in.feed_end();
@@ -49,15 +49,15 @@ TEST_CASE("Count event", "[count_event]") {
         }
 
         SECTION("Emit after") {
-            auto in = feed_input<event_set<input_event>>(
-                count_event<input_event, reset_event, output_event, true>(
+            auto in = feed_input<event_set<tick_event>>(
+                count_event<tick_event, fire_event, reset_event, true>(
                     0, 1, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
             in.feed_end();
             REQUIRE(out.check_end());
         }
@@ -65,31 +65,31 @@ TEST_CASE("Count event", "[count_event]") {
 
     SECTION("Threshold 1, limit 1") {
         SECTION("Emit before") {
-            auto in = feed_input<event_set<input_event>>(
-                count_event<input_event, reset_event, output_event, false>(
+            auto in = feed_input<event_set<tick_event>>(
+                count_event<tick_event, fire_event, reset_event, false>(
                     1, 1, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
             in.feed_end();
             REQUIRE(out.check_end());
         }
 
         SECTION("Emit after") {
-            auto in = feed_input<event_set<input_event>>(
-                count_event<input_event, reset_event, output_event, true>(
+            auto in = feed_input<event_set<tick_event>>(
+                count_event<tick_event, fire_event, reset_event, true>(
                     1, 1, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            REQUIRE(out.check(output_event{42}));
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            REQUIRE(out.check(output_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            REQUIRE(out.check(fire_event{42}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            REQUIRE(out.check(fire_event{42}));
             in.feed_end();
             REQUIRE(out.check_end());
         }
@@ -97,50 +97,50 @@ TEST_CASE("Count event", "[count_event]") {
 
     SECTION("Threshold 1, limit 2") {
         SECTION("Emit before") {
-            auto in = feed_input<event_set<input_event, reset_event>>(
-                count_event<input_event, reset_event, output_event, false>(
+            auto in = feed_input<event_set<tick_event, reset_event>>(
+                count_event<tick_event, fire_event, reset_event, false>(
                     1, 2, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            in.feed(input_event{43});
-            REQUIRE(out.check(output_event{43}));
-            REQUIRE(out.check(input_event{43}));
-            in.feed(input_event{44});
-            REQUIRE(out.check(input_event{44}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            in.feed(tick_event{43});
+            REQUIRE(out.check(fire_event{43}));
+            REQUIRE(out.check(tick_event{43}));
+            in.feed(tick_event{44});
+            REQUIRE(out.check(tick_event{44}));
             in.feed(reset_event{});
             REQUIRE(out.check(reset_event{}));
-            in.feed(input_event{45});
-            REQUIRE(out.check(input_event{45}));
-            in.feed(input_event{46});
-            REQUIRE(out.check(output_event{46}));
-            REQUIRE(out.check(input_event{46}));
+            in.feed(tick_event{45});
+            REQUIRE(out.check(tick_event{45}));
+            in.feed(tick_event{46});
+            REQUIRE(out.check(fire_event{46}));
+            REQUIRE(out.check(tick_event{46}));
             in.feed_end();
             REQUIRE(out.check_end());
         }
 
         SECTION("Emit after") {
-            auto in = feed_input<event_set<input_event, reset_event>>(
-                count_event<input_event, reset_event, output_event, true>(
+            auto in = feed_input<event_set<tick_event, reset_event>>(
+                count_event<tick_event, fire_event, reset_event, true>(
                     1, 2, ref_processor(out)));
             in.require_output_checked(out);
 
-            in.feed(input_event{42});
-            REQUIRE(out.check(input_event{42}));
-            REQUIRE(out.check(output_event{42}));
-            in.feed(input_event{43});
-            REQUIRE(out.check(input_event{43}));
-            in.feed(input_event{44});
-            REQUIRE(out.check(input_event{44}));
-            REQUIRE(out.check(output_event{44}));
+            in.feed(tick_event{42});
+            REQUIRE(out.check(tick_event{42}));
+            REQUIRE(out.check(fire_event{42}));
+            in.feed(tick_event{43});
+            REQUIRE(out.check(tick_event{43}));
+            in.feed(tick_event{44});
+            REQUIRE(out.check(tick_event{44}));
+            REQUIRE(out.check(fire_event{44}));
             in.feed(reset_event{});
             REQUIRE(out.check(reset_event{}));
-            in.feed(input_event{45});
-            REQUIRE(out.check(input_event{45}));
-            REQUIRE(out.check(output_event{45}));
-            in.feed(input_event{46});
-            REQUIRE(out.check(input_event{46}));
+            in.feed(tick_event{45});
+            REQUIRE(out.check(tick_event{45}));
+            REQUIRE(out.check(fire_event{45}));
+            in.feed(tick_event{46});
+            REQUIRE(out.check(tick_event{46}));
             in.feed_end();
             REQUIRE(out.check_end());
         }
