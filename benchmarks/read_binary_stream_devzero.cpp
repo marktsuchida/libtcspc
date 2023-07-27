4aad85e16fb531a60f3d2e1cde2b3ae3209b73c6
Mark A. Tsuchida <matsuchida@wisc.edu>
2023-07-27 17:14:29 -0500

    Move stream creation out of benchmark loop

--- benchmarks/read_binary_stream_devzero.cpp
+++ benchmarks/read_binary_stream_devzero.cpp
@@ -19,6 +19,32 @@ namespace tcspc {
 
 namespace {
 
+// Access input stream via reference, to ensure stream creation is not
+// dominant. (Effect was moderate for 1 MiB reads.)
+template <typename InputStream> class ref_input_stream {
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+    InputStream &stream;
+
+  public:
+    explicit ref_input_stream(InputStream &stream) : stream(stream) {}
+
+    auto is_error() noexcept -> bool { return stream.is_error(); }
+    auto is_eof() noexcept -> bool { return stream.is_eof(); }
+    auto is_good() noexcept -> bool { return stream.is_good(); }
+
+    auto tell() noexcept -> std::optional<std::uint64_t> {
+        return stream.tell();
+    }
+
+    auto skip(std::uint64_t bytes) noexcept -> bool {
+        return stream.skip(bytes);
+    }
+
+    auto read(span<std::byte> buffer) noexcept -> std::uint64_t {
+        return stream.read(buffer);
+    }
+};
+
 class unoptimized_null_sink {
   public:
     template <typename Event> void handle_event(Event const &event) noexcept {
@@ -30,52 +56,51 @@ class unoptimized_null_sink {
     }
 };
 
-static constexpr std::size_t total_bytes = 1 << 20;
+constexpr std::size_t total_bytes = 1 << 20;
 
 } // namespace
 
 void ifstream_unbuf(benchmark::State &state) {
+    auto stream =
+        internal::unbuffered_binary_ifstream_input_stream("/dev/zero");
     auto pool = std::make_shared<object_pool<std::vector<int>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream =
-            internal::unbuffered_binary_ifstream_input_stream("/dev/zero");
-        auto src =
-            read_binary_stream<int>(std::move(stream), total_bytes, pool,
-                                    state.range(0), unoptimized_null_sink());
+        auto src = read_binary_stream<int>(ref_input_stream(stream),
+                                           total_bytes, pool, state.range(0),
+                                           unoptimized_null_sink());
         src.pump_events();
     }
 }
 
 void ifstream(benchmark::State &state) {
+    auto stream = internal::binary_ifstream_input_stream("/dev/zero");
     auto pool = std::make_shared<object_pool<std::vector<int>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream = internal::binary_ifstream_input_stream("/dev/zero");
-        auto src =
-            read_binary_stream<int>(std::move(stream), total_bytes, pool,
-                                    state.range(0), unoptimized_null_sink());
+        auto src = read_binary_stream<int>(ref_input_stream(stream),
+                                           total_bytes, pool, state.range(0),
+                                           unoptimized_null_sink());
         src.pump_events();
     }
 }
 
 void cfile_unbuf(benchmark::State &state) {
+    auto stream = internal::unbuffered_binary_cfile_input_stream("/dev/zero");
     auto pool = std::make_shared<object_pool<std::vector<int>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream =
-            internal::unbuffered_binary_cfile_input_stream("/dev/zero");
-        auto src =
-            read_binary_stream<int>(std::move(stream), total_bytes, pool,
-                                    state.range(0), unoptimized_null_sink());
+        auto src = read_binary_stream<int>(ref_input_stream(stream),
+                                           total_bytes, pool, state.range(0),
+                                           unoptimized_null_sink());
         src.pump_events();
     }
 }
 
 void cfile(benchmark::State &state) {
+    auto stream = internal::binary_cfile_input_stream("/dev/zero");
     auto pool = std::make_shared<object_pool<std::vector<int>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream = internal::binary_cfile_input_stream("/dev/zero");
-        auto src =
-            read_binary_stream<int>(std::move(stream), total_bytes, pool,
-                                    state.range(0), unoptimized_null_sink());
+        auto src = read_binary_stream<int>(ref_input_stream(stream),
+                                           total_bytes, pool, state.range(0),
+                                           unoptimized_null_sink());
         src.pump_events();
     }
 }
--- benchmarks/write_binary_stream_devnull.cpp
+++ benchmarks/write_binary_stream_devnull.cpp
@@ -20,63 +20,80 @@ namespace tcspc {
 
 namespace {
 
-static constexpr std::size_t total_bytes = 1 << 20;
+// Access output stream via reference, to ensure stream creation is not
+// dominant. (Effect was moderate for 1 MiB writes.)
+template <typename OutputStream> class ref_output_stream {
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+    OutputStream &stream;
 
-}
+  public:
+    explicit ref_output_stream(OutputStream &stream) : stream(stream) {}
+
+    auto is_error() noexcept -> bool { return stream.is_error(); }
+
+    auto tell() noexcept -> std::optional<std::uint64_t> {
+        return stream.tell();
+    }
+
+    void write(span<std::byte const> buffer) noexcept { stream.write(buffer); }
+};
+
+constexpr std::size_t total_bytes = 1 << 20;
+
+} // namespace
 
 void ofstream_unbuf(benchmark::State &state) {
+    auto stream =
+        internal::unbuffered_binary_ofstream_output_stream("/dev/null");
     auto const write_size = state.range(0);
     auto const num_writes = total_bytes / write_size;
     std::vector<std::byte> data(write_size);
     auto pool = std::make_shared<object_pool<std::vector<std::byte>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream =
-            internal::unbuffered_binary_ofstream_output_stream("/dev/null");
-        auto proc = write_binary_stream(std::move(stream), pool, write_size,
-                                        null_sink());
+        auto proc = write_binary_stream(ref_output_stream(stream), pool,
+                                        write_size, null_sink());
         for (auto n = num_writes; n > 0; --n)
             proc.handle_event(autocopy_span(data));
     }
 }
 
 void ofstream(benchmark::State &state) {
+    auto stream = internal::binary_ofstream_output_stream("/dev/null");
     auto const write_size = state.range(0);
     auto const num_writes = total_bytes / write_size;
     std::vector<std::byte> data(write_size);
     auto pool = std::make_shared<object_pool<std::vector<std::byte>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream = internal::binary_ofstream_output_stream("/dev/null");
-        auto proc = write_binary_stream(std::move(stream), pool, write_size,
-                                        null_sink());
+        auto proc = write_binary_stream(ref_output_stream(stream), pool,
+                                        write_size, null_sink());
         for (auto n = num_writes; n > 0; --n)
             proc.handle_event(autocopy_span(data));
     }
 }
 
 void cfile_unbuf(benchmark::State &state) {
+    auto stream = internal::unbuffered_binary_cfile_output_stream("/dev/null");
     auto const write_size = state.range(0);
     auto const num_writes = total_bytes / write_size;
     std::vector<std::byte> data(write_size);
     auto pool = std::make_shared<object_pool<std::vector<std::byte>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream =
-            internal::unbuffered_binary_cfile_output_stream("/dev/null");
-        auto proc = write_binary_stream(std::move(stream), pool, write_size,
-                                        null_sink());
+        auto proc = write_binary_stream(ref_output_stream(stream), pool,
+                                        write_size, null_sink());
         for (auto n = num_writes; n > 0; --n)
             proc.handle_event(autocopy_span(data));
     }
 }
 
 void cfile(benchmark::State &state) {
+    auto stream = internal::binary_cfile_output_stream("/dev/null");
     auto const write_size = state.range(0);
     auto const num_writes = total_bytes / write_size;
     std::vector<std::byte> data(write_size);
     auto pool = std::make_shared<object_pool<std::vector<std::byte>>>();
     for ([[maybe_unused]] auto _ : state) {
-        auto stream = internal::binary_cfile_output_stream("/dev/null");
-        auto proc = write_binary_stream(std::move(stream), pool, write_size,
-                                        null_sink());
+        auto proc = write_binary_stream(ref_output_stream(stream), pool,
+                                        write_size, null_sink());
         for (auto n = num_writes; n > 0; --n)
             proc.handle_event(autocopy_span(data));
     }
