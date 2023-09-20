d83b8a343ba0466b48007970d379874ed2fe7956
Mark A. Tsuchida <matsuchida@wisc.edu>
2023-09-20 14:10:32 -0500

    Add processor context and tracker
    A mechanism to make it possible to retrieve data from processors after
    the processing pipeline has run (or during a run), despite the fact that
    the processors are embedded (moved) into the upstream source and cannot
    be directly accessed.

---
+++ include/libtcspc/processor_context.hpp
@@ -0,0 +1,208 @@
+/*
+ * This file is part of libtcspc
+ * Copyright 2019-2023 Board of Regents of the University of Wisconsin System
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <any>
+#include <cassert>
+#include <functional>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+namespace tcspc {
+
+class processor_context;
+
+/**
+ * \brief Tracker object that mediates access to processors via a \c
+ * processor_context.
+ *
+ * \ingroup misc
+ *
+ * Instances should be obtained from a \ref processor_context. A processor
+ * stores the instance as a data member and also registers an accessor factory
+ * in its constructor. This allows code other than upstream processors to later
+ * access the processor state.
+ *
+ * \see processor_context
+ *
+ * \tparam Accessor type of accessor interface for processor
+ */
+template <typename Accessor> class processor_tracker {
+    std::shared_ptr<processor_context> ctx; // Null iff 'empty' state.
+    std::string name;
+    std::function<Accessor(processor_tracker &)> accessor_factory;
+
+    friend class processor_context;
+
+    // Construct only via friend processor_context.
+    explicit processor_tracker(std::shared_ptr<processor_context> context,
+                               std::string name)
+        : ctx(std::move(context)), name(std::move(name)) {}
+
+  public:
+    /**
+     * \brief Default constructor.
+     *
+     * Constructs in the "empty" state, unconnected to a processor context.
+     * Empty instances cannot be used other than by assigning a non-empty
+     * instance.
+     */
+    processor_tracker() = default;
+
+    // Noncopyable.
+    processor_tracker(processor_tracker const &) = delete;
+    auto operator=(processor_tracker const &) = delete;
+
+    /** \brief Move constructor. */
+    processor_tracker(processor_tracker &&other) noexcept;
+
+    /** \brief Move assignment operator. */
+    auto operator=(processor_tracker &&rhs) noexcept -> processor_tracker &;
+
+    /** \brief Destructor. */
+    ~processor_tracker();
+
+    /**
+     * \brief Register an accessor factory with this tracker's context.
+     *
+     * This is usually called in the processors constructor to arrange for
+     * later access to the processor via an accessor.
+     *
+     * \tparam F factory function type
+     *
+     * \param factory factory function taking reference to this tracker and
+     * returning the accessor of type \c Accessor
+     */
+    template <typename F> void register_accessor_factory(F factory) {
+        assert(ctx);
+        accessor_factory = std::move(factory);
+    }
+};
+
+/**
+ * \brief Context for enabling access to processors after they have been
+ * incorporated into a processing chain.
+ *
+ * \ingroup misc
+ *
+ * Instances must be handled by \c std::shared_ptr.
+ *
+ * A processor context mediates access to the state of individual processors
+ * from outside of the processing chain/pipeline. This is done by means of \c
+ * processor_tracker objects that are obtained from the context and need to be
+ * embedded into processors when constructing the latter. The tracker moves and
+ * is destroyed together with the processor, allowing the context to track how
+ * to access the processor after being moved into the processing chain.
+ *
+ * Each processor/tracker is associated a name, which must be unique within a
+ * given context (and may not be reused even after destroying the corresponding
+ * processor/tracker).
+ *
+ * Actual access to processor state is through an \e accessor object whose type
+ * is defined by the processor and whose instances can be obtained from the
+ * context by giving the processor name.
+ *
+ * \see processor_tracker
+ */
+class processor_context
+    : public std::enable_shared_from_this<processor_context> {
+    // Map keys are processor names and values are pointer to
+    // processor_tracker<Accessor> for some Accessor. Values are kept up to
+    // date when tracker is moved or destroyed. If the tracked processor has
+    // been destroyed, the entry remains and the value is an empty std::any()).
+    // Reuse of name is not allowed.
+    std::unordered_map<std::string, std::any> trackers;
+
+    template <typename Accessor> friend class processor_tracker;
+
+    template <typename Accessor>
+    void update_tracker_address(std::string const &processor_name,
+                                processor_tracker<Accessor> *ptr) {
+        // Enforce no type change in std::any; just update the value directly.
+        *std::any_cast<processor_tracker<Accessor> *>(
+            &trackers.at(processor_name)) = ptr;
+    }
+
+  public:
+    processor_context() = default;
+    processor_context(processor_context const &) = delete;
+    auto operator=(processor_context const &) = delete;
+    processor_context(processor_context &&) = delete;
+    auto operator=(processor_context &&) = delete;
+    ~processor_context() = default;
+
+    /**
+     * \brief Obtain a tracker for a processor with the given name.
+     *
+     * \tparam Accessor the processor's accessor type
+     *
+     * \param processor_name name to assign to the tracked processor
+     *
+     * \return tracker to be stored in the processor
+     */
+    template <typename Accessor>
+    auto tracker(std::string processor_name) -> processor_tracker<Accessor> {
+        if (trackers.count(processor_name) > 0)
+            throw std::logic_error(
+                "cannot create tracker for existing processor name in context");
+        auto ret = processor_tracker<Accessor>(shared_from_this(),
+                                               std::move(processor_name));
+        trackers.insert({ret.name, std::any(&ret)});
+        return ret;
+    }
+
+    /**
+     * \brief Obtain an accessor for the named processor.
+     *
+     * The returned accessor is only valid while the processor is alive and
+     * unmoved, so it should not be stored by the caller.
+     *
+     * \tparam Accessor the accessor type (documented by the processor)
+     *
+     * \param processor_name the processor name
+     */
+    template <typename Accessor>
+    auto accessor(std::string const &processor_name) -> Accessor {
+        auto tracker_ptr = std::any_cast<processor_tracker<Accessor> *>(
+            trackers.at(processor_name));
+        if (tracker_ptr == nullptr)
+            throw std::logic_error(
+                "cannot access destroyed processor through context");
+        return tracker_ptr->accessor_factory(*tracker_ptr);
+    }
+};
+
+template <typename Accessor>
+processor_tracker<Accessor>::processor_tracker(
+    processor_tracker &&other) noexcept
+    : ctx(std::move(other.ctx)), name(std::move(other.name)),
+      accessor_factory(std::move(other.accessor_factory)) {
+    if (ctx)
+        ctx->update_tracker_address(name, this);
+}
+
+template <typename Accessor>
+auto processor_tracker<Accessor>::operator=(processor_tracker &&rhs) noexcept
+    -> processor_tracker & {
+    ctx = std::move(rhs.ctx);
+    name = std::move(rhs.name);
+    accessor_factory = std::move(rhs.accessor_factory);
+    if (ctx)
+        ctx->update_tracker_address(name, this);
+    return *this;
+}
+
+template <typename Accessor>
+processor_tracker<Accessor>::~processor_tracker() {
+    if (ctx)
+        ctx->update_tracker_address<Accessor>(name, nullptr);
+}
+
+} // namespace tcspc
---
+++ test/processor_context.cpp
@@ -0,0 +1,123 @@
+/*
+ * This file is part of libtcspc
+ * Copyright 2019-2023 Board of Regents of the University of Wisconsin System
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "libtcspc/processor_context.hpp"
+
+#include <catch2/catch_all.hpp>
+
+#include <cstddef>
+
+namespace tcspc {
+
+TEST_CASE("processor context and tracker", "[processor_context]") {
+    auto ctx = std::make_shared<processor_context>();
+
+    struct test_accessor {
+        void *tracker_addr;
+    };
+
+    CHECK_THROWS(ctx->accessor<test_accessor>("nonexistent"));
+
+    // Scope to test tracker destruction.
+    {
+        auto trk = ctx->tracker<test_accessor>("myproc");
+        trk.register_accessor_factory(
+            [](auto &tracker) { return test_accessor{&tracker}; });
+        CHECK(ctx->accessor<test_accessor>("myproc").tracker_addr == &trk);
+
+        // No duplicates.
+        CHECK_THROWS(ctx->tracker<test_accessor>("myproc"));
+
+        auto moved_trk = std::move(trk);
+        CHECK(ctx->accessor<test_accessor>("myproc").tracker_addr ==
+              &moved_trk);
+
+        processor_tracker<test_accessor> move_assigned_trk;
+        move_assigned_trk = std::move(moved_trk);
+        CHECK(ctx->accessor<test_accessor>("myproc").tracker_addr ==
+              &move_assigned_trk);
+    }
+
+    CHECK_THROWS(ctx->accessor<test_accessor>("myproc"));
+
+    // No duplicates even after destruction.
+    CHECK_THROWS(ctx->tracker<test_accessor>("myproc"));
+}
+
+namespace {
+
+struct example_access {
+    // An accessor shoulud be a single (unparameterized) type per processor
+    // template. Type erasure of the processor can be afforded by storing
+    // std::function instances for actual access to the processor.
+    std::function<int()> value;
+};
+
+class example_processor {
+    // In this example the processor is not a template, but most real
+    // processors have very long type names due to the downstream being
+    // included. To implement an accessor, it is best to use an alias type.
+    using self_type = example_processor;
+
+    // Processor data.
+    int value = 42;
+
+    // Downstream omitted from example.
+
+    // Cold data after downstream. The tracker should be here, since it is
+    // accessed at much lower frequency than the actual processing.
+    processor_tracker<example_access> trk;
+
+  public:
+    // Processors supporting context-base access should have a constructor that
+    // takes a tracker as its first parameter (and also an otherwise equivalent
+    // constructor that does not; not shown).
+    explicit example_processor(processor_tracker<example_access> &&tracker)
+        : trk(std::move(tracker)) {
+        trk.register_accessor_factory(
+            // We register a callable that can create an accessor given the
+            // tracker. The accessor is only valid while the processor (and
+            // therefore its tracker) stay alive in its current location, so
+            // within the callable we can make these assumptions.
+            [](processor_tracker<example_access> &t) {
+                // Note: offsetof() on non-standard-layout types is only
+                // "conditionally-supported" as of C++17. I expect this not to
+                // be a problem in practice, but it should be noted.
+                static constexpr std::size_t tracker_offset =
+                    offsetof(self_type, trk);
+                auto *self = reinterpret_cast<self_type *>(
+                    reinterpret_cast<std::byte *>(&t) - tracker_offset);
+                // Finally, we use lambda(s) to supply the type-erased
+                // function(s) by which the accessor interacts with the
+                // processor. As stated above, we assume 'self' remains valid
+                // during the lifetime of the accessor.
+                return example_access{[self]() -> int { return self->value; }};
+            });
+    }
+
+    // Event handlers and flush() omitted from example.
+};
+
+} // namespace
+
+TEST_CASE("processor tracker intended use", "[processor_context]") {
+    auto ctx = std::make_shared<processor_context>();
+    {
+        // The context is injected into a processor upon creation, when later
+        // access to the processor is desired.
+        auto proc =
+            example_processor(ctx->tracker<example_access>("test_proc"));
+
+        // Then, the processor can be accessed by name at a later time, even if
+        // the processor has been moved.
+        CHECK(ctx->accessor<example_access>("test_proc").value() == 42);
+        auto moved_proc = std::move(proc);
+        CHECK(ctx->accessor<example_access>("test_proc").value() == 42);
+    }
+    CHECK_THROWS(ctx->accessor<example_access>("test_proc"));
+}
+
+} // namespace tcspc
