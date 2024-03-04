47dca36e07efee1909ba8d5e0380396f6c819f29
Mark A. Tsuchida <matsuchida@wisc.edu>
2024-03-04 14:32:37 -0600

    Basic Python (cppyy) setup
    Get packaging and installation working, and set up include path so that
    the packaged C++ headers can be accessed with cppyy.
    
    Also set project version (0.1.0.dev0) so that pyproject.toml can fetch
    it.

---
+++ python/libtcspc/__init__.py
@@ -0,0 +1,24 @@
+# This file is part of libtcspc
+# Copyright 2019-2024 Board of Regents of the University of Wisconsin System
+# SPDX-License-Identifier: MIT
+
+import importlib.resources
+
+import cppyy
+
+from ._version import __version__ as __version__
+
+# In order to get the include directory for our C++ headers, we assume that the
+# headers are real files that can be located relative to tcspc.hpp. This will
+# not work with unusual import methods such as zipimport, but neither do
+# extension modules (including cppyy's backend), so nothing is really lost.
+# This does work with meson-python's editable install.
+
+with importlib.resources.as_file(
+    importlib.resources.files("libtcspc").joinpath(
+        "include/libtcspc/tcspc.hpp"
+    )
+) as main_header:
+    cppyy.add_include_path(str(main_header.parent.parent))
+
+cppyy.include("libtcspc/tcspc.hpp")
