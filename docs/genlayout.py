0f3eca37808eae21d7abb37f1b3c5a474f8cba1a
Mark A. Tsuchida <matsuchida@wisc.edu>
2024-04-13 11:28:14 -0500

    Display detailed description at top of page
    Auto-generate the modified DoxygenLayout.xml, so that it is otherwise
    the default from the version of Doxygen being used.

---
+++ docs/genlayout.py
@@ -0,0 +1,36 @@
+#!python3
+# This file is part of libtcspc
+# Copyright 2019-2024 Board of Regents of the University of Wisconsin System
+# SPDX-License-Identifier: MIT
+
+import sys
+import xml.etree.ElementTree as ET
+
+infile, outfile = sys.argv[1:]
+
+tree = ET.parse(infile)
+root = tree.getroot()
+
+assert root.tag == "doxygenlayout"
+
+# For each section, remove {detailed,brief}description and re-add
+# detaileddescription at top. Skip section if it does not contain
+# detaileddescription (navindex).
+
+for section in root:
+    had_dd = False
+    for dd in section.findall("detaileddescription"):
+        had_dd = True
+        section.remove(dd)
+    if not had_dd:
+        continue
+    had_bd = False
+    for bd in section.findall("briefdescription"):
+        had_bd = True
+        section.remove(bd)
+    assert had_bd
+    section.insert(
+        0, ET.Element("detaileddescription", {"title": "Description"})
+    )
+
+tree.write(outfile)
